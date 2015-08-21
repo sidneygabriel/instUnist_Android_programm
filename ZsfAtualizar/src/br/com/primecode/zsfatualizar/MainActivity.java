e65b10bcee53eb9909e12dd1db6747e3ccbb25b6
sidneygabriel <sidneygabriel@gmail.com>
2015-08-21 16:20:02 -0300

    commit

---
+++ ZsfAtualizar/src/br/com/primecode/zsfatualizar/MainActivity.java
@@ -0,0 +1,62 @@
+package br.com.primecode.zsfatualizar;
+
+import java.io.File;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+	private Button unistall;
+	private Button install;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		unistall = (Button) findViewById(R.id.btnUnis);
+		install = (Button) findViewById(R.id.btnIns);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	public void unistall(View view) {
+		
+		Intent intent = new Intent(Intent.ACTION_DELETE);
+		intent.setData(Uri.parse("package:br.com.sismobile"));
+		startActivity(intent);
+	}
+	
+	public void install(View view){
+		
+		Intent intent = new Intent(Intent.ACTION_VIEW);
+	    intent.setDataAndType(Uri.fromFile(new File(Environment.getExternalStorageDirectory() + "/sismobile/import/apk/" + "PrimeSF.apk")), "application/vnd.android.package-archive");
+	    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+	    startActivity(intent);
+	}
+	
+}
